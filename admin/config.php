7c7cf1a002f49026ef6e2722a1befba9c234ee18
Donnmyth <Amit2cha@gmail.com>
2018-02-03 00:08:19 -0800

    complete app
    app

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://www.farmstore.me/admin/');
+define('HTTP_CATALOG', 'https://www.farmstore.me/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://www.farmstore.me/admin/');
+define('HTTPS_CATALOG', 'https://www.farmstore.me/');
+
+// DIR
+define('DIR_APPLICATION', '/home/donn/public_html/admin/');
+define('DIR_SYSTEM', '/home/donn/public_html/system/');
+define('DIR_LANGUAGE', '/home/donn/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/donn/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/donn/public_html/system/config/');
+define('DIR_IMAGE', '/home/donn/public_html/image/');
+define('DIR_CACHE', '/home/donn/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/donn/public_html/system/download/');
+define('DIR_UPLOAD', '/home/donn/public_html/system/upload/');
+define('DIR_LOGS', '/home/donn/public_html/system/logs/');
+define('DIR_MODIFICATION', '/home/donn/public_html/system/modification/');
+define('DIR_CATALOG', '/home/donn/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'donn_opcrt');
+define('DB_PASSWORD', 'Amit@123');
+define('DB_DATABASE', 'donn_opcrt');
+define('DB_PREFIX', 'oc_');
